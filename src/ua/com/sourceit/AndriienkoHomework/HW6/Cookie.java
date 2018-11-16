288debe74993c20deef63577bc4422cdcd13af11
Oleksii Andriienko <alex.v.andrienko@gmail.com>
2018-11-17 00:22:51 +0200

    HW6.Add cookie.

---
+++ src/ua/com/sourceit/AndriienkoHomework/HW6/Cookie.java
@@ -0,0 +1,10 @@
+package ua.com.sourceit.AndriienkoHomework.HW6;
+
+public class Cookie extends Sweets {
+
+	public Cookie(String name, double weight, double sugarContent) {
+		super(name, sugarContent, weight);
+		
+	}
+
+}
