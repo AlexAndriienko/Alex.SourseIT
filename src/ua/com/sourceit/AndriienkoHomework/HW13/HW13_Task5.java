df00cc5abde16ba7868f538c29e374776a2ceec8
Oleksii Andriienko <alex.v.andrienko@gmail.com>
2018-12-19 21:50:46 +0200

    Add HW13_5

---
+++ src/ua/com/sourceit/AndriienkoHomework/HW13/HW13_Task5.java
@@ -0,0 +1,93 @@
+package ua.com.sourceit.AndriienkoHomework.HW13;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class HW13_Task5 {
+
+	public static void main(String[] args) {
+		HW13_Task5_Resourse res = new HW13_Task5_Resourse();
+		ExecutorService executor = Executors.newFixedThreadPool(4);
+
+		Thread writer = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				int i = 1;
+				while (i < 4) {
+					try {
+						System.out.println("writer: " + i);
+						res.writeBuffer("11111");
+						i++;
+						Thread.sleep(2);
+					} catch (InterruptedException e) {
+					} finally {
+
+					}
+
+				}
+			}
+
+		});
+
+		Thread reader1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				int i = 1;
+				while (i < 4) {
+
+					try {
+						System.out.println("reader1: " + i + " " + res.readBuffer());
+						i++;
+						Thread.sleep(3);
+					} catch (InterruptedException e) {
+					} finally {
+					}
+				}
+			}
+
+		});
+
+		Thread reader2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				int i = 1;
+				while (i < 4) {
+
+					try {
+						System.out.println("reader2: " + i + " " + res.readBuffer());
+						i++;
+						Thread.sleep(3);
+					} catch (InterruptedException e) {
+					} finally {
+					}
+				}
+			}
+
+		});
+		
+		Thread reader3 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				int i = 1;
+				while (i < 4) {
+
+					try {
+						System.out.println("reader3: " + i + " " + res.readBuffer());
+						i++;
+						Thread.sleep(3);
+					} catch (InterruptedException e) {
+					} finally {
+					}
+				}
+			}
+
+		});
+
+		executor.submit(writer);
+		executor.submit(reader1);
+		executor.submit(reader2);
+		executor.submit(reader3);
+
+	}
+
+}
