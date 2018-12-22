18899b20affae46f4867876087bcec256e82d71b
Oleksii Andriienko <alex.v.andrienko@gmail.com>
2018-12-22 02:19:26 +0200

    Start test

---
+++ src/ua/com/sourceit/AndriienkoHomework/HW1/HW1task3.java
@@ -0,0 +1,24 @@
+package ua.com.sourceit.AndriienkoHomework.HW1;
+//Part3
+//�������� �����, ������� ��������� ���������������� ����������� ����������� ������ �������� ���� ����� ������������� �����, 
+//���������� � ���������� ��� ��������� ��������� ������.
+
+public class HW1task3 {
+	public static void main(String[] args) {
+		
+		int x = Integer.parseInt(args[0]);
+		int y = Integer.parseInt(args[1]);
+		int i;
+		while (y != 0) {
+			i = x % y;
+			x = y;
+			y = i;
+		}		
+		System.out.println("���������� ����� �������� �����: " + x);	
+	}
+
+}
+//cmd
+//javac C:\Users\Alex\eclipse-workspace\SourceIT_Andriienko\src\hw1task3.java
+//java -cp C:\Users\Alex\eclipse-workspace\SourceIT_Andriienko\src\ hw1task3 55 15
+//30
